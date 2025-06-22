ce1b221de400e45dff29e452528309d80b9f5650
mauro <mauro_biazutti@hotmail.com>
2025-06-22 00:53:25 -0300

    fix: config bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
+import * as Popper from "@popperjs/core"
 import * as bootstrap from "bootstrap"
-import  "popper" 
-import  "bootstrap" 
+import "controllers"
