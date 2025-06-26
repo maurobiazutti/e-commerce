a531364fae89bee13b56bcaf4266ff647eb57d3f
mauro <mauro_biazutti@hotmail.com>
2025-06-26 11:13:38 -0300

    test: create test model user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
   factory :user do
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    password_confirmation { password }
+    name { Faker::Name.name }
+    phone { Faker::PhoneNumber.phone_number }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
-require 'faker'
 
-# RSpec.describe User, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe User, type: :model do
+  context "Espero validar campos obligatorios" do
+    it "Valida campos" do
+      user = build(:user)
+      expect(user.valid?). to eq(true)
+    end
+
+    it "Valida campos nome vazio" do
+      user = build(:user, name: nil)
+      expect(user.valid?).to eq(false)
+    end
+  end
+end
