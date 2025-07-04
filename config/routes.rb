5fd504d1e08a4087ede991990a8c842b1d1a9bc4
mauro <mauro_biazutti@hotmail.com>
2025-07-04 11:31:51 -0300

    feat(admin): develop controller/ home, base, create routes admin #29

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root "home#index"
   devise_for :users
   # get "up" => "rails/health#show", as: :rails_health_check
+  
+  root "home#index"
+  namespace :admin do
+    root "home#index"
+  end
+
 end
